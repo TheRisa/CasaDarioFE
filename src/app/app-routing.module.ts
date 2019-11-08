6dfb632e7ffa92019160f6fd0c0930a224a62bbc
DRisaliti <dariorisa@hotmail.it>
2019-11-08 19:17:17 +0100

    Feat/header html (#2)
    * feat(login): html init
    
    * crea html dell header
    
    * aggiunge back
    
    * cambia colore in base a parametro passato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   {
     path: 'loading',
     loadChildren: './loading/loading.module#LoadingComponentModule'
   },
-  { path: 'calendario', loadChildren: './calendario/calendario.module#CalendarioPageModule' }
+  {
+    path: 'calendario',
+    loadChildren: './calendario/calendario.module#CalendarioPageModule'
+  }
 ];
 @NgModule({
   imports: [

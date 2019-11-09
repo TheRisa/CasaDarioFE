a339b910cab9f7a49ef43a9d473a6d6b9fd737bd
TheRisa <dariorisa@hotmail.it>
2019-11-09 20:54:25 +0100

    develop html del footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+/**
+ * Classe per la gestione del componente footer
+ */
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  @Input() cose: string;
+  /**
+   * Flag che indica se lo sfondo è rosso
+   */
+  @Input() isRed: string;
 
+  /**
+   * Costruttore della classe
+   */
   constructor() {}
 
+  /**
+   * Metodo on init della classe
+   */
   ngOnInit() {}
 }

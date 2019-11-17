1bb9249d4aa124baf4bc711749b9ed893b5f7802
TheRisa <dariorisa@hotmail.it>
2019-11-17 02:50:21 +0100

    crea html ask me

---
+++ src/app/ask-me/ask-me.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ask-me',
+  templateUrl: './ask-me.page.html',
+  styleUrls: ['./ask-me.page.scss'],
+})
+export class AskMePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

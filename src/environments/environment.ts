a6a6977a2543b702ae3e0b88427387e27bea6803
TheRisa <dariorisa@hotmail.it>
2020-01-04 03:06:33 +0100

    serverwq

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  // httpRequestUrl: 'https://polar-tundra-64747.herokuapp.com/api/'
+  httpRequestUrl: 'https://polar-tundra-64747.herokuapp.com/api/'
   // httpRequestUrl: 'http://192.168.1.1:8000/api/',
-  httpRequestUrl: 'http://localhost:8000/api/'
+  // httpRequestUrl: 'http://localhost:8000/api/'
 };
 
 /*
